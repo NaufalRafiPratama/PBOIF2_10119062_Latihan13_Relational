cb628bd2694c6a5d36d6e28696d6648b37d97ca1
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-10-20 17:30:43 +0700

    Add files via upload

---
+++ PBOIF2_10119062_Latihan13_Relational/src/pboif2_10119062_latihan13_relational/PBOIF2_10119062_Latihan13_Relational.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119062_latihan13_relational;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis
+ * tipe data bilangan bulat 
+ */
+public class PBOIF2_10119062_Latihan13_Relational {
+
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
